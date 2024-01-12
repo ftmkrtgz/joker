7128a90c940f15ce6a391d4c15d1d0c902ecefa9
ftmkrtgz <salihagemici.123@gmail.com>
2024-01-12 10:08:17 +0100

    added new code in joker.test file

--- src/js/joker.test.js
+++ src/js/joker.test.js
@@ -9,3 +9,14 @@ export async function testDadJoke() {
 }
 
 testDadJoke();
+export function dadJoke() {
+    return new Promise(function (resolve, reject) {
+        try {
+            joker.getRandomDadJoke(function (joke) {
+                resolve(joke);
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
